19b46aea9a86ea786b5f42b2c23e61cdc7bef548
Albert <32515582+albertmilosz@users.noreply.github.com>
2017-10-04 15:40:55 +0200

    0.0

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 0,x;
+            int[] a = { 10, 12, 14 };
+            for (x = 0; x <= 2; x++)
+            {
+                y += 2 * x;
+                
+                
+                for(int i = 0; i < a.Length; i++)
+                {
+                    y += 2 * a[i];
+                }
+                Console.WriteLine(y);
+            }
+
+            Console.WriteLine(y);
+        }
+    }
+}
