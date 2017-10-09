33c5b220038aaedd6e728753a9266d6ef9ee13e4
Albert <32515582+albertmilosz@users.noreply.github.com>
2017-10-09 14:14:29 +0200

    0.2

--- Dyktando.cs
+++ Dyktando.cs
@@ -10,36 +10,47 @@ namespace Dyktando
     {
         static void Main(string[] args)
         {
-            
-            string[] tab1 = {"górnik","lekarz" };
-            string[] tab2 = {"g*rnik", "leka*"};
+
+            string[] tab1 = { "górnik", "lekarz", "helikopter", "herbata","krowa" };
+            string[] tab2 = { "g*rnik", "leka*", "*elikopter", "*erbata", "k**wa" };
             string[] bledy = new string[tab2.Length];
-            string slowo;
-            int punkty = 0;
+            string[] bledy_zapisane = new string[tab2.Length];
+            string slowo,slowo_lower;
+            int punkty = 0,x = 0;
 
             for (int i = 0; i < tab1.Length; i++)
             {
                 Console.WriteLine("uzupełnij poprawnie luki w słowie: " + tab2[i]);
                 slowo = Convert.ToString(Console.ReadLine());
-                if (slowo == tab1[i])
+                slowo_lower = slowo.ToLower();
+                if (slowo_lower == tab1[i])
                 {
                     punkty++;
                 }
                 else
                 {
-                    bledy[i] = tab1[i];
+                   
+                    bledy_zapisane[x] = slowo;
+                    bledy[x] = tab1[i];
+                    x++;
                 }
             }
-            Console.WriteLine("Uzyskałeś {0,1} pkt. Gratulacje!", punkty);
+            Console.WriteLine("\nUzyskałeś {0,1} pkt.", punkty);
 
-            if (bledy[] = 0)
+            if (x > 0)
             {
-                Console.WriteLine("\npoprawne odpowiedzi to: ");
-                
-                foreach (string i in bledy)
+                Console.WriteLine("\nIlość błędów {0,1}", x);
+                Console.WriteLine("\nPopełniłeś błąd w: ");
+
+               for ( int i = 0; i < x; i++)
                 {
-                    Console.WriteLine(i);
+                    
+                    Console.WriteLine("Twoja odpowiedź: {0,1} poprawna: {1,1}", bledy_zapisane[i], bledy[i]);
                 }
+            } else
+            {
+                
+                Console.WriteLine("Udało Ci się rozwiązać dyktando bezbłędnie! Brawo!");
             }
             Console.ReadKey();
 
