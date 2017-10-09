03513fcf8a08a9a30c716cafce1ccccef7bc126b
Albert <32515582+albertmilosz@users.noreply.github.com>
2017-10-09 16:32:19 +0200

    0.1

---
+++ Ciąg Fibonacciego.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0, y = 0, z = 0;
+
+           
+
+            for (x = 1, y = 1;z < 1000; )
+            {
+                
+                z = x + y;
+                y = x;
+                x = z;
+
+                Console.WriteLine(z);
+            }
+           
+        }
+    }
+}
