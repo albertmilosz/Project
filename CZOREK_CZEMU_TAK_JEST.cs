2c44678d281705b38ae96ffb860a676dad9e5b69
Albert <32515582+albertmilosz@users.noreply.github.com>
2017-10-10 19:17:29 +0200

    0.1

---
+++ CZOREK_CZEMU_TAK_JEST.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void metoda(out int x, out int y)
+        {
+            x = 2;
+            y = 3;
+            Console.WriteLine("{0,1} {1,1}", x, y);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 5, b = 2;
+            Console.WriteLine("{0,1} {1,1}", a,b);
+            metoda(out a, out b);
+            Console.WriteLine("{0,1} {1,1}", a,b);
+
+        }
+    }
+}
